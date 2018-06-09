0c0b8e556cbbdad7ac051b5bcef07feb63d18611
radomboontaksana <40064233+radomboontaksana@users.noreply.github.com>
2018-06-09 03:01:26 -0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'vCW7Zm0423iaNtI2HMhR+MyAP0q8LFGM/4IPk4I68nySkY8k7ThvkxDwEpkwXSSXAxGHOBRWnTTkaahrCcJFM5mwbeibKd1g3stxKQYTCn9A5qXOa2K18Y2cfYCmJT6hrV+MFeT6BEwnmBGveiR2NQdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
